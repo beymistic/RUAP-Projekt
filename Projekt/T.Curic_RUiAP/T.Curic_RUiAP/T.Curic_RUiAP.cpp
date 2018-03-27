00681dfdd25d0dbc62af7d3d31024529326ac898
beymistic <tomislav.dm@gmail.com>
2018-03-27 13:26:42 +0200

    Izmjena source fajla, br. indeksa

--- Projekt/T.Curic_RUiAP/T.Curic_RUiAP/T.Curic_RUiAP.cpp
+++ Projekt/T.Curic_RUiAP/T.Curic_RUiAP/T.Curic_RUiAP.cpp
@@ -7,7 +7,7 @@
 int main()
 {
 	//char sacekusa;
-	printf("Pozdrav iz aplikacije od Tomislava Æuriæ");
+	printf("Pozdrav iz aplikacije od Tomislava Æuriæ, a sto se br. indeksa tice, nisam skolovala");
 	//scanf_s("%s", sacekusa);
     return 0;
 }
